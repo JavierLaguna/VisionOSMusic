e78b2b69c00e07cdd7f473e4aa2595c2fe93a03b
Javier Laguna <javier.laguna@innocv.com>
2023-08-27 19:17:34 +0200

    DrumPieceView WIP

--- VisionOSMusic/Scenes/Instruments/DrumDemo/DrumPieceView.swift
+++ VisionOSMusic/Scenes/Instruments/DrumDemo/DrumPieceView.swift
@@ -7,26 +7,51 @@ struct DrumPieceView: View {
     let type: DrumKitPieceComponent.PieceType
     let onPressPlayButton: () -> Void
     
+    @State private var isExpanded = false
+    
     var body: some View {
-        VStack(alignment: .leading) {
-            HStack {
-                Image("snare_image")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 60, height: 60)
-                
-                Text("Snare")
-                    .font(.title)
+        Group {
+            if isExpanded {
+                VStack(alignment: .leading) {
+                    HStack {
+                        Image("snare_image")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 60, height: 60)
+                        
+                        Text("Snare")
+                            .font(.title)
+                    }
+                    .padding(.top, 8)
+                    .padding(.horizontal, 4)
+                    
+                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+                        .padding()
+                        .background(.regularMaterial)
+                        .clipShape(.rect(cornerRadius: 12))
+                    
+                    Button("Play", action: onPressPlayButton)
+                }
+            } else {
+                HStack {
+                    Image("snare_image")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 60, height: 60)
+                    
+                    Text("Snare")
+                        .font(.title)
+                    
+                    Spacer()
+                    
+                    Image(systemName: "rectangle.arrowtriangle.2.outward")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+//                    Image(systemName: "rectangle.arrowtriangle.2.inward")
+                }
+                .padding(.top, 8)
+                .padding(.horizontal, 4)
             }
-            .padding(.top, 8)
-            .padding(.horizontal, 4)
-            
-            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
-                .padding()
-                .background(.regularMaterial)
-                .clipShape(.rect(cornerRadius: 12))
-            
-            Button("Play", action: onPressPlayButton)
         }
         .padding()
         .padding(.bottom, 12)
