bcc230ba2845857ff89ae266332dfc40b91dcdc9
Javier Laguna <javier.laguna@innocv.com>
2023-08-31 20:06:10 +0200

    More WIP today

--- VisionOSMusic/Scenes/Instruments/SnareDrum/SnareDrumView.swift
+++ VisionOSMusic/Scenes/Instruments/SnareDrum/SnareDrumView.swift
@@ -5,24 +5,48 @@ import RealityKitContent
 
 struct SnareDrumView: View {
     
+    @State private var viewModel = SnareDrumViewModel()
+    
     var body: some View {
         ZStack {
             SnareModelView()
+                .environment(viewModel)
          
             ModelControlsView()
                 .offset(y: 124)
+                .environment(viewModel)
         }
     }
 }
 
 private struct SnareModelView: View {
     
+    @Environment(SnareDrumViewModel.self) private var viewModel
+    
+    @State private var entity: Entity?
+    
     var body: some View {
-        Model3D(named: Scene3D.snareDrum, bundle: realityKitContentBundle)
+        RealityView { content in
+            do {
+                let rootEntity = try await Entity(named: Scene3D.snareDrum, in: realityKitContentBundle)
+                entity = rootEntity
+//                rootEntity.position = SIMD3<Float>(0, 0, -2)
+//                content.add(rootEntity)
+//                
+//                subscriptions.append(content.subscribe(to: ComponentEvents.DidAdd.self, componentType: DrumKitPieceComponent.self, { event in
+//                    createDrumKitPieceView(for: event.entity)
+//                }))
+                
+            } catch {
+                fatalError("Failed to load a model asset.")
+            }
+        }
     }
 }
 
 private struct ModelControlsView: View {
+    
+    @Environment(SnareDrumViewModel.self) private var viewModel
     
     @State private var isPickerVisible: Bool = false
     
---
+++ VisionOSMusic/Scenes/Instruments/SnareDrum/SnareDrumViewModel.swift
@@ -0,0 +1,10 @@
+
+import Observation
+import RealityKit
+
+@Observable
+final class SnareDrumViewModel {
+    
+    var entity: Entity?
+    var rotateIsOn = false
+}
